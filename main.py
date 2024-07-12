68d24e0d2bda1041f930cd907634fae8b1e2c1d8
Vladislav Merkodanov <94777515+Tw1nreverb@users.noreply.github.com>
2024-07-12 15:53:19 +0400

    add movement, fps, bg_fill

--- main.py
+++ main.py
@@ -5,13 +5,31 @@ pygame.init()
 screen_width = 1280
 screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
+FPS = 60
+clock = pygame.time.Clock()
 run = True
-
-player = Player(500, 500, 0.1)
+moving_left = False
+moving_right = False
+player = Player(500, 300, 0.1, 5)
 while run:
-    screen.blit(player.img, player.rect)
+    clock.tick(FPS)
+    screen.fill((123, 4, 5))
+    player.draw(screen)
+    player.move(moving_left, moving_right)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
     pygame.display.update()
 pygame.quit()
--- player.py
+++ player.py
@@ -3,10 +3,31 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, scale):
+    def __init__(self, x, y, scale, speed):
         super().__init__()
         img = pygame.image.load('static/player.png')
         self.img = pygame.transform.scale(
             img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-        self.rect = img.get_rect()
+        self.rect = self.img.get_rect()
         self.rect.center = (x, y)
+        self.speed = speed
+        self.flip = False
+        self.direction = 1
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+        if moving_left:
+            dx = -self.speed
+            self.flip = False
+            self.direction = 1
+        if moving_right:
+            dx = self.speed
+            self.flip = True
+            self.direction = -1
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self, screen):
+        screen.blit(pygame.transform.flip(self.img, self.flip, False),
+                    self.rect)
