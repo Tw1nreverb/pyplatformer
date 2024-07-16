c2469daf360b37447111db964c2076990118f9f4
Vladislav Merkodanov <94777515+Tw1nreverb@users.noreply.github.com>
2024-07-16 21:23:49 +0400

    add collision

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from player import Player, Coin
 from functions import draw_text
 import button
 import inputbox
@@ -7,6 +6,86 @@ from database.db import register_user, login_user
 import csv
 #Constans
 pygame.init()
+gravity = 0.45
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, scale, speed):
+        super().__init__()
+        img = pygame.image.load('static/player.png')
+        self.img = pygame.transform.scale(
+            img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.img.get_rect()
+        self.rect.center = (x, y)
+        self.speed = speed
+        self.flip = False
+        self.coin = 0
+        self.direction = 1
+        self.jump = False
+        self.life = True
+        self.vel_y = 0
+        self.in_air = False
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+        if moving_left:
+            dx = -self.speed
+            self.flip = False
+            self.direction = 1
+        if moving_right:
+            dx = self.speed
+            self.flip = True
+            self.direction = -1
+        if self.jump == True and not (self.in_air):
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+        self.vel_y += gravity
+        if self.vel_y > 10:
+            self.vel_y
+        dy += int(self.vel_y)
+        for tile in world.obstacle_list:
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width,
+                                   self.height):
+                dx = 0
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width,
+                                   self.height):
+                if self.vel_y < 0:
+                    self.vel_y = 0
+                    dy = tile[1].bottom - self.rect.top
+                elif self.vel_y >= 0:
+                    self.vel_y = 0
+                    self.in_air = False
+                    dy = tile[1].top - self.rect.bottom
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self, screen):
+        screen.blit(pygame.transform.flip(self.img, self.flip, False),
+                    self.rect)
+
+
+class Coin(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, player):
+        super().__init__()
+        self.player = player
+        self.image = pygame.image.load('static/coin.png')
+        self.image = pygame.transform.scale(self.image, (int(
+            self.image.get_width() * 1), int(self.image.get_height() * 1)))
+        self.rect = self.image.get_rect()
+        self.rect.midtop = (x + 50 // 2, y + (50 - self.image.get_height()))
+
+    def update(self):
+        if pygame.sprite.collide_rect(self, self.player):
+            self.player.coin += 1
+            self.kill()
+
+
 pygame.display.set_caption("Summer Practice platformer")
 screen_width = 1280
 screen_height = 720
@@ -100,7 +179,7 @@ class Decoration(pygame.sprite.Sprite):
 
 
 decoration_group = pygame.sprite.Group()
-player = Player(500, 300, 0.1, 5)
+player = Player(500, 300, 1.5, 5)
 coin_group = pygame.sprite.Group()
 water_group = pygame.sprite.Group()
 #input boxes
